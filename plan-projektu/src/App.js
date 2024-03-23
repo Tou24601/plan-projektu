a49286b9ec793b4d1a60c7dd5adeddd5a8d38bf6
joannakolakowska <kolakowska.joanna.jk@gmail.com>
2024-03-23 20:06:45 +0100

    started

--- plan-projektu/src/App.js
+++ plan-projektu/src/App.js
@@ -1,26 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Karuzela from './components/Component';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload it.
-        </p>
-<<<<<<< Updated upstream
-=======
-    <h1>Witajcie w naszej bajce</h1>
->>>>>>> Stashed changes
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <Karuzela />
       </header>
     </div>
   );
